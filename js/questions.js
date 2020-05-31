d35163f41966b787d1bff1621e54fa81439645ab
Shaurya0802 <62699422+Shaurya0802@users.noreply.github.com>
2020-05-31 21:32:24 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,17 @@
+class Questions{
+    constructor(){}
+
+    getQuestions(){
+        var getQuestionInfo = database.ref('questions');
+        getQuestionInfo.on("value",(data)=>{
+            allQuestions = data.val();
+        })
+    }      
+
+    static getEachQuestion(questionNum){
+        var getQuestion = database.ref('questions' + questionNum);
+        getQuestion.on("value",function(data){
+            question = data.val();
+        })
+    }
+}
